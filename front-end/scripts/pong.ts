5931ac1df0d59de0708db76e89fc6f8c50e5d6e4
Semen Ganiev <sam.edu.post42@gmail.com>
2025-06-26 17:56:15 +0200

    moved welcome page functions to different file

--- front-end/scripts/pong.ts
+++ front-end/scripts/pong.ts
@@ -1,4 +1,5 @@
 import { initGameOptionHighlight } from './choose-mode.js';
+import { setInitialWelcomePage, addHashForChooseModePage } from './welcome-page.js';
 
 // for the welcome page:
 setInitialWelcomePage();
@@ -6,16 +7,3 @@ addHashForChooseModePage();
 
 // for the choose mode page:
 initGameOptionHighlight();
-
-function setInitialWelcomePage() {
-  if (!location.hash)
-    location.hash = 'welcome-page';
-}
-
-function addHashForChooseModePage() {
-  const playButtonEl = document.querySelector('.js-play-button');
-
-  playButtonEl?.addEventListener('click', () => {
-    location.hash = 'choose-mode-page';
-  });
-}
