00c1abd3041c30408c2a5d51e245343b603d43ef
Carlos Sydney Steuding <csteudin@2-D-2.42heilbronn.de>
2025-09-23 18:18:43 +0200

    removed VAULT, added monitoring system with grafana & prometheus

--- docker/database/init.sql
+++ docker/database/init.sql
@@ -1,8 +1,4 @@
--- Erstellt Tabelle "users", falls noch nicht vorhanden
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL
 );
-
--- Optional: Dummy-Einträge für Tests
-INSERT INTO users (name) VALUES ('Alice'), ('Bob');
