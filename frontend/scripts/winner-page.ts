3945bb4279a7696e865544e2ed69db1032aa30cd
Eleni Perperidou <elperperidou@gmail.com>
2026-01-06 19:53:25 +0100

    deleting comments

--- frontend/scripts/winner-page.ts
+++ frontend/scripts/winner-page.ts
@@ -9,14 +9,6 @@ export function initWinnerAnnouncementPage(tSettings: TournamentSettings): void 
     console.log("First place :", tSettings.firstPlaceAlias);
     console.log("Second place :", tSettings.secondPlaceAlias);
     const winnerSection = document.getElementById('winner-page');
-    
-    // document.querySelectorAll('section').forEach(section => {
-    //     section.style.display = 'none';
-    // });
-    
-    // if (winnerSection) {
-    //     winnerSection.style.display = 'block'; 
-    // }
 
     const firstPlaceElement = tSettings.firstPlaceAlias;
     const secondPlaceElement = tSettings.secondPlaceAlias;
@@ -32,32 +24,7 @@ export function initWinnerAnnouncementPage(tSettings: TournamentSettings): void 
     }
 }
 
-// function displayWinners(firstPlaceElement: string | null, secondPlaceElement: string | null): void {
-    
-//     const hash = location.hash;
-    
-//     const queryString = hash.includes('?') ? hash.substring(hash.indexOf('?') + 1) : '';
-//     const urlParams = new URLSearchParams(queryString);
-    
-//     const firstPlace = urlParams.get('first'); 
-//     const secondPlace = urlParams.get('second'); 
-
-//     if (firstPlace && firstPlaceElement) {
-//         firstPlaceElement.textContent = `1st Place: ${decodeURIComponent(firstPlace)}`;
-//     } else if (firstPlaceElement) {
-//         firstPlaceElement.textContent = `1st Place: `;
-//     }
-
-//     if (secondPlace && secondPlaceElement) {
-//         secondPlaceElement.textContent = `2nd Place: ${decodeURIComponent(secondPlace)}`;
-//     } else if (secondPlaceElement) {
-//         secondPlaceElement.textContent = `2nd Place: `;
-//     }
-// }
-
 function displayWinners(firstPlaceName: string, secondPlaceName: string): void {
-    // Βρίσκουμε τα στοιχεία στο HTML όπου θα γραφτούν τα ονόματα
-    // Αντικατάστησε το 'first-place-id' με το πραγματικό ID που έχεις στο HTML σου
     console.log("First place from display:", firstPlaceName);
     console.log("Second place from display:", secondPlaceName);
 
