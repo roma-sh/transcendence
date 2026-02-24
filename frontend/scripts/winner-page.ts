3035ad6b97a7583e12a2242080e883022e001b38
Eleni Perperidou <eleni@iMac.fritz.box>
2026-02-24 15:46:57 +0100

    cleaning a bit

--- frontend/scripts/winner-page.ts
+++ frontend/scripts/winner-page.ts
@@ -1,24 +1,3 @@
-// import { TournamentSettings } from "./types.js";
-
-// export function handleGoToWelcomePage() {
-//     location.hash = '#welcome-page';
-// }
-
-// export function initWinnerAnnouncementPage(tSettings: TournamentSettings): void {
-
-//     const firstPlaceEl = document.getElementById('first-place-winner') as HTMLElement | null;
-//     const secondPlaceEl = document.getElementById('second-place-winner') as HTMLElement | null;
-
-//     const { firstPlaceAlias, secondPlaceAlias } = tSettings;
-
-//     if (firstPlaceEl) {
-//         firstPlaceEl.textContent = `1st Place: ${firstPlaceAlias || ''}`;
-//     }
-//     if (secondPlaceEl) {
-//         secondPlaceEl.textContent = `2nd Place: ${secondPlaceAlias || ''}`;
-//     }
-// }
-
 import { TournamentSettings } from "./types.js";
 
 export function handleGoToWelcomePage() {
